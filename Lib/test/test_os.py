9c5c950d3e35e094988edf6543053059bc99be11
Russell Keith-Magee <russell@keith-magee.com>
2024-02-05 08:04:57 +0800

    [3.11] gh-114099: Add test exclusions to support running the test suite on iOS (#114889)
    Add test annotations required to run the test suite on iOS (PEP 730).
    
    The majority of the change involve annotating tests that use subprocess,
    but are skipped on Emscripten/WASI for other reasons, and including
    iOS/tvOS/watchOS under the same umbrella as macOS/darwin checks.
    
    `is_apple` and `is_apple_mobile` test helpers have been added to
    identify *any* Apple platform, and "any Apple platform except macOS",
    respectively.

--- Lib/test/test_os.py
+++ Lib/test/test_os.py
@@ -3750,6 +3750,7 @@ class TermsizeTests(unittest.TestCase):
         self.assertGreaterEqual(size.columns, 0)
         self.assertGreaterEqual(size.lines, 0)
 
+    @support.requires_subprocess()
     def test_stty_match(self):
         """Check if stty returns the same results
 
