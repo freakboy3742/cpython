641a8d5893686663d249e70b74181883fd6751f0
John Zhou <john-xyz@outlook.com>
2025-04-18 13:07:20 -0500

    Add more visionos references that were missing.

--- Lib/ctypes/util.py
+++ Lib/ctypes/util.py
@@ -126,7 +126,7 @@ if os.name == "nt":
                         if (name := _get_module_filename(h)) is not None]
         return libraries
 
-elif os.name == "posix" and sys.platform in {"darwin", "ios", "tvos", "watchos"}:
+elif os.name == "posix" and sys.platform in {"darwin", "ios", "tvos", "watchos", "visionos"}:
     from ctypes.macholib.dyld import dyld_find as _dyld_find
     def find_library(name):
         possible = ['lib%s.dylib' % name,
