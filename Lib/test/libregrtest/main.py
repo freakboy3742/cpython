e6af9a3c8775465c191a3e79e1005a35dac5bd57
Russell Keith-Magee <russell@keith-magee.com>
2026-01-30 09:56:12 +0800

    [3.10] Add support for tvOS and watchOS.

--- Lib/test/libregrtest/main.py
+++ Lib/test/libregrtest/main.py
@@ -676,7 +676,8 @@ class Regrtest:
         except SystemExit as exc:
             # bpo-38203: Python can hang at exit in Py_Finalize(), especially
             # on threading._shutdown() call: put a timeout
-            faulthandler.dump_traceback_later(EXIT_TIMEOUT, exit=True)
+            if self.ns.timeout is not None:
+                faulthandler.dump_traceback_later(EXIT_TIMEOUT, exit=True)
 
             sys.exit(exc.code)
 
